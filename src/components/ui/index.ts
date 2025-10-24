f4e78df1c0c72f06f87264fdf6278828221ecfb6
bencacic <bencacic@gmail.com>
2025-10-24 14:37:09 -0600

    Implement dashboard layout as new home page

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,3 +2,5 @@ export { Button } from "./Button";
 export { Input } from "./Input";
 export { Textarea } from "./Textarea";
 export { IconButton } from "./IconButton";
+export { DashboardCard } from "./DashboardCard";
+export { Tab, Tabs } from "./Tabs";
